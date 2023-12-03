80f2b27b8ba688d0ce629b27d88f6160789e9d86
Ward <143821199+ahmdWard@users.noreply.github.com>
2023-12-03 19:46:12 +0200

    Update file_reader.py

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,12 +1,30 @@
 class FileReader:
-    
-    def __init__(self, fileName, opcode = False, instruction= True) -> None:
-        self.file = open(fileName, 'r')
+
+    def __init__(self, fileName, opcode=False, instruction=True) -> None:
+        self.file_name = fileName
+        self.opcode_flag = opcode
+        self.instruction_flag = instruction
+        self.file = None
         self.instructions = []
-        self.opCode = opcode
-        self.instruction = instruction
+        self.opcodes = []
+
+    def __enter__(self):
+        try:
+            self.file = open(self.file_name, 'r')
+            return self
+        except FileNotFoundError:
+            print(f"Error: File '{self.file_name}' not found.")
+            return None
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.file:
+            self.file.close()
+            print("File closed")
 
     def read(self) -> None:
+        if not self.file:
+            return
+
         content = self.file.read()
         content = content.split("\n")
         
@@ -14,27 +32,22 @@ class FileReader:
             if line == '':
                 continue
             self.parse(line)
-    
+
     def parse(self, line):
-        instruction = []
-        line = line.split(" ", 2)
-        for i in line:
-            if i == '':
-                continue
-            i.strip(" ")
-            instruction.append(i)
-        
-        self.instructions.append(instruction)
-    
+        parts = line.split(" ", 2)
+        parts = [part.strip() for part in parts if part.strip()]
+
+        if self.instruction_flag and len(parts) > 1:
+            self.instructions.append(parts)
+
+        if self.opcode_flag and len(parts) > 0:
+            self.opcodes.append(parts[0])
+
     def getInstructions(self):
         return self.instructions
     
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.file.close()
-        print("File closed")
+    def getOpcodes(self):
+        return self.opcodes
 
 
 # TODO : change read method to read opcode or instructions
