b78f611433a3ca3c4cab93e1bd2e97923c62e108
Ward <143821199+ahmdWard@users.noreply.github.com>
2023-12-04 00:58:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from src.sic_assembler import PcGenerator,Assembler
 
 
 def main():
-    fileName = '/home/dekuu5/Code/myProjects/faculty-projects/sic-assembler/testEx/test1.txt'
-    with FileReader(fileName) as fileReader:
+    fileName = '\\Users\\ahmed\Downloads\\sic-assembler-main (1)\\sic-assembler-main\\testEx\\test1.txt'
+    with FileReader(fileName, False, True) as fileReader:
         fileReader.read()
         instructions = fileReader.getInstructions()
-
-    for i in instructions:
-        print(i)
+    
     pcGenerator = PcGenerator(instructions)
     pcGenerator.generate()
     instructions = pcGenerator.getInstructions()
     labelMap = pcGenerator.getLabelMap()
     for i in instructions:
         print(i)
-    # assembler = Assembler(instructions,labelMap)
-    # assembler.generateObjectCode()
-    # Assembler.Save("ob1.txt")
+    assembler = Assembler(instructions,labelMap)
+    assembler.generateObjectCode()
+    assembler.save("result//ob1.txt") #create a folder  for result ignore it in testing:" !!!!!!
+    objectCode = assembler.getObjectCode()
+    print(objectCode)
+    
     
 
 if __name__ == "__main__":
