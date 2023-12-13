18f5a4506b00088d7ee0ab9d3ab7a8c95893f61d
alaa2702 <74565333+alaa2702@users.noreply.github.com>
2023-12-13 18:29:22 +0200

    done2

--- src/sic_assembler.py
+++ src/sic_assembler.py
@@ -131,12 +131,12 @@ class Assembler:
                 obj.append(item)
             else:
                 index -=len(item)
-                text=f"T^{j}^{hex(int(index/2))[2:]}^{'^'.join(obj)}"
+                text=f"T^{j.zfill(6)}^{hex(int(index/2))[2:].zfill(2)}^{'^'.join(obj)}"
                 textRecord.append(text)
                 j=self.instructions[i+1][0][2:]
                 obj=[item]
                 index=len(item)
-        text=f"T^{j}^{hex(int(index/2))[2:]}^{'^'.join(obj)}"
+        text=f"T^{j.zfill(6)}^{hex(int(index/2))[2:].z}^{'^'.join(obj)}"
         textRecord.append(text)
         t='\n'.join(textRecord)
         self.FinalObjectCode = f"{header}\n{t}\n{end}"
