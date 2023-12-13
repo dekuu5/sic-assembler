8291840810f48e76506ff5952eeae73e7007388a
alaa2702 <74565333+alaa2702@users.noreply.github.com>
2023-12-13 18:05:48 +0200

    done

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from src.sic_assembler import PcGenerator,Assembler
 
 
 def main():
-    fileName = '/home/dekuu5/Code/myProjects/faculty-projects/sic-assembler/testEx/test1.txt'
+    fileName = r'C:\Users\Alaa\Source\Repos\dekuu5\sic-assembler\testEx\test1.txt'
     with FileReader(fileName, False, True) as fileReader:
         fileReader.read()
         instructions = fileReader.getInstructions()
     
-    opCodeFile = '/home/dekuu5/Code/myProjects/faculty-projects/sic-assembler/testEx/opCode.txt'
+    opCodeFile = r'C:\Users\Alaa\Source\Repos\dekuu5\sic-assembler\testEx\opCode.txt'
     with FileReader(opCodeFile, True, False) as fileReaderOpcode:
         fileReaderOpcode.read()
         opCodeMap = fileReaderOpcode.getOpcodes()
